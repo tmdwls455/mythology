93fd3bc20c1fc9ac6599ed9d413c3aa7d548e03b
admin <tmdwls455@gmail.com>
2024-01-28 00:24:48 +0900

    테스트

--- src/test/java/com/mythology/MythologyApplicationTests.java
+++ src/test/java/com/mythology/MythologyApplicationTests.java
@@ -1,13 +1,44 @@
 package com.mythology;
 
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class MythologyApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
+	@Autowired
+	private SqlSessionFactory sessionFactory;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() {}
+
+	@Test
+	public void testApplicationContext() {
+		try {
+			System.out.println("=========================================");
+			System.out.println(context.getBean("sqlSessionFactory"));
+			System.out.println("=========================================");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void testSqlSessionFactory() {
+		try {
+			System.out.println("=========================================");
+			System.out.println(sessionFactory.toString());
+			System.out.println("=========================================");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
