51debe358251ede3c8b9336b78d95641ce2c519c
admin <tmdwls455@gmail.com>
2024-02-11 14:59:16 +0900

    테스트

--- src/main/java/com/mythology/controller/BoardController.java
+++ src/main/java/com/mythology/controller/BoardController.java
@@ -1,10 +1,12 @@
 package com.mythology.controller;
 
+import com.mythology.domain.BoardDTO;
 import com.mythology.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class BoardController {
@@ -13,14 +15,16 @@ public class BoardController {
     private BoardService boardService;
 
     @GetMapping("/board/write.do")
-    public String openBoardWrite(Model model) {
-        String title = "타이틀";
-        String content = "내용";
-        String writer = "저자";
-
-        model.addAttribute("title", title);
-        model.addAttribute("content", content);
-        model.addAttribute("writer", writer);
+    public String openBoardWrite(@RequestParam(value = "idx", required = false) Long bno, Model model) {
+        if (bno == null) {
+            model.addAttribute(("board"), new BoardDTO());
+        } else {
+            BoardDTO board = boardService.getBoardDetail(bno);
+            if (board == null) {
+                return "redirect:/board/list.do";
+            }
+            model.addAttribute("board", board);
+        }
 
         return "board/write";
     }
